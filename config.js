e2ed8b814556869f48c31e1dbea60273e823751d
Mohammed Samy <mosamy19@gmail.com>
2020-06-15 13:56:51 +0200

    final commit

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        "clientId": '330975977473819',
+        "clientSecret": '319c57abfab073fd19aa96dd91fd15db'
+    }
+}
--- routes/leaderRouter.js
+++ routes/leaderRouter.js
@@ -1,35 +1,70 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const authenticate = require('../authenticate');
+const cors = require('./cors');
+
+const Leaders = require('../modals/leaders');
+
 const leaderRouter = express.Router();
 
 leaderRouter.use(bodyParser.json());
-leaderRouter.route('/').all((req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
+leaderRouter.route('/')
+.options(cors.corsWithOptions, (req, res) => { res.sendStatus (200) })
+.get(cors.cors, (req, res, next) => {
+    Leaders.find({}).then((leaders) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(leaders);
+    }, (err) => next(err)).catch((err) => next(err));
 })
-.get((req, res, next) => {
-    res.end("Will send all the leaders to you");
-})
-.post((req, res, next) => {
-    res.end("Will add the leader " + req.body.name 
-    + ' with details ' + req.body.description);
-}).put((req, res, next) => {
+.post(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+    Leaders.create(req.body).then((leader) => {
+        console.log('Leader Created ', leader);
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(leader);
+    }, (err) => next(err)).catch((err) => next(err));
+}).put(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
     res.statusCode = 403;
     res.end("PUT opreation not supported on /leaders")
-}).delete((req, res, next) => {
-    res.end("Deleting all the leaders");
+}).delete(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+    Leaders.remove({})
+            .then((resp) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(resp);
+            }, (err) => next(err)).catch((err) => next(err));
 });
 
-leaderRouter.route('/:leaderId').get((req, res, next) => {
-    res.end("Will send details of the leader: " + req.params.leaderId + " to you");
-}).post((req, res, next) => {
+leaderRouter.route('/:leaderId')
+.options(cors.corsWithOptions, (req, res) => { res.sendStatus (200) })
+.get(cors.cors, (req, res, next) => {
+    Leaders.findById(req.params.leaderId).then((leader) => {
+        console.log('Leader Created ', leader);
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(leader);
+    }, (err) => next(err)).catch((err) => next(err))
+}).post(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
     res.statusCode = 403;
-    res.end("POST opreation not supported on /leades/" + + req.params.leaderId);
-}).put((req, res, next) => {
-    res.write("Updating the leader " + req.params.leaderId + '\n')
-    res.end("Will update the leader " + req.body.name + ' with details: ' + req.body.description);
-}).delete((req, res, next) => {
-    res.end("Deleting leader: " + req.params.leaderId);
+    res.end("POST opreation not supported on /leades/" + req.params.leaderId);
+}).put(cors.corsWithOptions,authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+    Leaders.findByIdAndUpdate(req.params.leaderId, {
+        $set: req.body
+    }, { new: true })
+    .then((leader) => {
+        console.log('Leader Created ', leader);
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(leader);
+    }, (err) => next(err)).catch((err) => next(err))
+}).delete(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+    Leaders.findByIdAndRemove(req.params.leaderId)
+                .then((resp) => {
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'application/json');
+                    res.json(resp);
+                }, (err) => next(err)).catch((err) => next(err));
 });
 module.exports = leaderRouter;
